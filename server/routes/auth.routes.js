18b7acf2d7588a3220b2637c4ec0873efa0fbd7a
ashgolan <alaa.t.shaalan@gmail.com>
2025-11-07 22:25:29 +0200

    change ratelimit file name

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,14 +1,17 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/auth.controller.js";
 import { validateRegistration } from "../middlewares/validate.middleware.js";
-import { apiLimiter, loginLimiter } from "../middlewares/rateLimit.middleware.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import {
+  apiLimiter,
+  loginLimiter,
+} from "../middlewares/rateLimit.middleware.js";
 
 const router = express.Router();
 
 router.post(
   "/register",
-  authMiddleware,          // ✅ أضف هذا السطر
+  authMiddleware, // ✅ أضف هذا السطر
   apiLimiter,
   validateRegistration,
   registerUser
