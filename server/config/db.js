954b1305fc0e610a8f5a095819fb24ef6c7cac7d
ashgolan <alaa.t.shaalan@gmail.com>
2025-11-08 21:50:15 +0200

    update mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,16 @@
+// config/db.js
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      tls: true,
+      tlsAllowInvalidCertificates: true,
+      serverSelectionTimeoutMS: 20000,
+    });
     console.log("✅ MongoDB connected successfully");
-  } catch (error) {
-    console.error("❌ MongoDB connection failed:", error);
+  } catch (err) {
+    console.error("❌ MongoDB connection failed:", err);
     process.exit(1);
   }
 };
