06f7a0ffebf5817683b2c4c1af4981ed353e40a6
ashgolan <alaa.t.shaalan@gmail.com>
2025-11-08 18:21:24 +0200

    change api code

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -2,13 +2,14 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const Api = axios.create({
-  baseURL:
-    process.env.REACT_APP_API || "http://localhost:4000",
+  baseURL: "/",
 });
 
 Api.interceptors.request.use((config) => {
   const token = Cookies.get("JWT");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
