bb8e29658664857fcad612a5da2e53642d1bf5bd
ashgolan <alaa.t.shaalan@gmail.com>
2025-11-09 12:08:57 +0200

    restore storage bucket name

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCyoxmVKLzHdgibJ0GXhk5JMevenP8_vAY",
   authDomain: "fateness-364c3.firebaseapp.com",
   projectId: "fateness-364c3",
-  storageBucket: "fateness-364c3.firebasestorage.app",
+storageBucket: "fateness-364c3.appspot.com",
   messagingSenderId: "282672405307",
   appId: "1:282672405307:web:1a52b192177662997d351e",
   measurementId: "G-EEGVQVCRH8",
