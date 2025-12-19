a7623d7c7421cf586d3bd3d5dcfc892b21464c2f
ashgolan <alaa.t.shaalan@gmail.com>
2025-12-19 11:31:46 +0200

    change Api.js

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
+const baseURL =
+  process.env.REACT_APP_API_URL || "http://localhost:4000";
+
 const Api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL,
 });
 
 Api.interceptors.request.use((config) => {
