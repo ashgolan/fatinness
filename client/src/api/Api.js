b4fc493b24f0b8f7bc71d013acd83dede315e313
ashgolan <alaa.t.shaalan@gmail.com>
2025-12-19 11:52:15 +0200

    change api.js

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -1,18 +1,32 @@
+// import axios from "axios";
+// import Cookies from "js-cookie";
+
+// let baseURL;
+
+// // Local
+// if (window.location.hostname === "localhost") {
+//   baseURL = "http://localhost:4000";
+// } else {
+//   // Production
+//   baseURL = process.env.REACT_APP_API_URL || "https://api.fatinness.cloud";
+// }
+
+// const Api = axios.create({
+//   baseURL,
+// });
+
+// Api.interceptors.request.use((config) => {
+//   const token = Cookies.get("JWT");
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// export { Api };
 import axios from "axios";
 import Cookies from "js-cookie";
 
-let baseURL;
-
-// Local
-if (window.location.hostname === "localhost") {
-  baseURL = "http://localhost:4000";
-} else {
-  // Production
-  baseURL = process.env.REACT_APP_API_URL || "https://api.fatinness.cloud";
-}
-
 const Api = axios.create({
-  baseURL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 Api.interceptors.request.use((config) => {
