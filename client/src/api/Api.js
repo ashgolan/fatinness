c0caf3f989b244be4ed78b5d6119bba4faf0004c
ashgolan <alaa.t.shaalan@gmail.com>
2025-12-19 11:19:31 +0200

    change Api.js

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -1,18 +1,8 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-let baseURL;
-
-// Local
-if (window.location.hostname === "localhost") {
-  baseURL = "http://localhost:4000";
-} else {
-  // Production
-  baseURL = process.env.REACT_APP_API_URL || "https://api.fatinness.cloud";
-}
-
 const Api = axios.create({
-  baseURL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 Api.interceptors.request.use((config) => {
