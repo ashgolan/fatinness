2077dc32c317abc20f28201f350348e2b3e11b01
ashgolan <alaa.t.shaalan@gmail.com>
2025-12-20 09:58:31 +0200

    change slot schema

--- server/models/Slot.js
+++ server/models/Slot.js
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 
 const SlotSchema = new mongoose.Schema({
   date: { type: Date, required: true },
-  startTime: { type: String, required: true },
-  endTime: { type: String, required: true },
+  startAt: { type: Date, required: true },
+  endAt: { type: Date, required: true },
   capacity: { type: Number, default: 20 },
   isBlocked: { type: Boolean, default: false },
   templateId: { type: mongoose.Schema.Types.ObjectId, ref: "WeekTemplate" },
   createdAt: { type: Date, default: Date.now },
 });
 
-SlotSchema.index({ date: 1, startTime: 1 }, { unique: true });
+SlotSchema.index({ startAt: 1, endAt: 1 }, { unique: true });
 
 const Slot = mongoose.model("Slot", SlotSchema);
 
