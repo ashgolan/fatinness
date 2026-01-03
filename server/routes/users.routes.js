a504498431940504f83b2c13827d5b5e5f7d9520
ashgolan <alaa.t.shaalan@gmail.com>
2026-01-03 09:51:06 +0200

    delete unused renewsubscription funcs and routes

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -8,7 +8,6 @@ import {
   transferFcmToThisDevice,
 } from "../controllers/users.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { renewSubscription } from "../controllers/users.controller.js";
 
 const router = express.Router();
 
@@ -19,7 +18,6 @@ router.put("/me", updateUserProfile);
 router.get("/me/weight-history", authMiddleware, getWeightHistory);
 router.post("/me/weight", authMiddleware, addWeightPoint);
 router.post("/fcm", updateFcmToken);
-router.post("/renew-subscription", authMiddleware, renewSubscription);
 
 router.post("/fcm/transfer", authMiddleware, transferFcmToThisDevice);
 
